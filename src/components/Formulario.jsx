c65cadb73bd7e8055b376920012a3d26d2556421
juanluna96 <juandluna17@gmail.com>
2021-02-11 12:14:05 -0500

    Leyendo lo que el usuario escribe en los inputs

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -13,9 +13,16 @@ const Formulario = () => {
 
     // Funcion que se ejecuta cada que el usuario escribe en un input
 
-    const actualizarState = () => {
-        console.log('escribiendo')
+    const actualizarState = (e) => {
+        actualizarCita({
+            ...cita,
+            [e.target.name]: e.target.value
+        })
     }
+
+    // Extraer los valores
+
+    const { mascota, propietario, fecha, hora, sintomas } = cita;
 
     return (
         <Fragment>
@@ -23,23 +30,23 @@ const Formulario = () => {
             <form method="post" action="">
                 <div className="form-group">
                     <label htmlFor="mascota">Nombre mascota</label>
-                    <input id="mascota" className="form-control" type="text" name="mascota" onChange={ actualizarState } />
+                    <input id="mascota" className="form-control" type="text" name="mascota" onChange={ actualizarState } value={ mascota } />
                 </div>
                 <div className="form-group">
                     <label htmlFor="propietario">Nombre dueño</label>
-                    <input id="propietario" className="form-control" type="text" name="propietario" onChange={ actualizarState } />
+                    <input id="propietario" className="form-control" type="text" name="propietario" onChange={ actualizarState } value={ propietario } />
                 </div>
                 <div className="form-group">
                     <label htmlFor="fecha">Fecha</label>
-                    <input id="fecha" className="form-control" type="date" name="fecha" onChange={ actualizarState } />
+                    <input id="fecha" className="form-control" type="date" name="fecha" onChange={ actualizarState } value={ fecha } />
                 </div>
                 <div className="form-group">
                     <label htmlFor="hora">Hora</label>
-                    <input id="hora" className="form-control" type="time" name="hora" onChange={ actualizarState } />
+                    <input id="hora" className="form-control" type="time" name="hora" onChange={ actualizarState } value={ hora } />
                 </div>
                 <div className="form-group">
                     <label htmlFor="sintomas">Síntomas</label>
-                    <textarea id="sintomas" className="form-control" name="sintomas" rows="3" onChange={ actualizarState } ></textarea>
+                    <textarea id="sintomas" className="form-control" name="sintomas" rows="3" onChange={ actualizarState } value={ sintomas } ></textarea>
                 </div>
                 <button className="btn btn-primary btn-block" type="submit">Agendar cita</button>
             </form>
