270465836b5716f1a7e8de25ae954606065e4761
juanluna96 <juandluna17@gmail.com>
2021-02-12 15:33:48 -0500

    Documentando con PropTypes

--- src/components/Cita.jsx
+++ src/components/Cita.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 const Cita = ({ cita, eliminarCita }) => {
     return (
@@ -14,6 +15,11 @@ const Cita = ({ cita, eliminarCita }) => {
             >Eliminar</button>
         </div>
     )
+}
+
+Cita.propTypes = {
+    cita: PropTypes.object.isRequired,
+    eliminarCita: PropTypes.func.isRequired
 }
 
 export default Cita
